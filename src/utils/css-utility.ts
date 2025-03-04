b7cb49d85b71f02cc0238cafe926719c91d70e4b
Gabriel <gabrielsc.dev@gmail.com>
2025-03-04 17:23:17 -0300

    bug fix

--- src/utils/css-utility.ts
+++ src/utils/css-utility.ts
@@ -1,15 +1,24 @@
-export function removeClass(targetClass: string) {
-    document.querySelectorAll(`.${targetClass}`).forEach(item => {
-        item.classList.remove(targetClass);
-    })
+export function removeClassesFrom(classes: string[], ref: HTMLTableElement | null) {
+    classes.forEach(cls => removeClassFrom(cls, ref));
 }
 
-export function removeClasses(classes: string[]) {
-    classes.forEach(removeClass);
+
+export function removeClassFrom(targetClass: string, ref: HTMLTableElement | null) {
+    if (ref === null) return;
+
+    ref.childNodes[1].childNodes
+        .forEach(row => row.childNodes
+            .forEach(cell => (cell.firstChild as Element)?.classList.remove(targetClass)))
 }
 
-export function addClassesTo(classes: string[], targetId: string) {
-    classes.forEach(item => {
-        document.getElementById(targetId)?.classList.add(item);
-    })
+export function addClassesTo(classes: string[], ref: HTMLTableElement | null, id: string) {
+    if (ref === null) return;
+    if (classes.length === 0) return;
+
+    ref.childNodes[1].childNodes
+        .forEach(row => row.childNodes
+            .forEach(cell => {
+                const _cell = cell.firstChild as Element;
+                if (_cell?.id === id ) {classes.forEach(cls => _cell?.classList.add(cls))}
+            }))
 }
